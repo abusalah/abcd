100b66c1eb181e9c1129aa814cada02fe7047e5b
bara <bara@ubuntu>
2014-07-09 08:33:19 -0400

    mynewclass

---
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/main/java/org/apache/hadoop/mapreduce/v2/app/MyNewClass.java
@@ -0,0 +1,15 @@
+package org.apache.hadoop.mapreduce.v2.app;
+
+public class MyNewClass {
+	
+	public MyNewClass(){System.out.println("_________________Entered constructor of MyNewClass.java");}
+	
+	public static int[] replicasHashes_555_set=new int[5];
+	
+	//public static void main(String[] args){
+		
+	//	System.out.println("_________________Entered main of MyNewClass.java");
+		
+	//}
+
+}
