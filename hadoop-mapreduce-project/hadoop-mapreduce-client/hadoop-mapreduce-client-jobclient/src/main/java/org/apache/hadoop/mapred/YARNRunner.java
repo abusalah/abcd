3bc05b3e4b458bc4d3cb2f48bd25d6e177aa884c
Bara_Ubuntu64bit <bara@ubuntu.(none)>
2014-08-25 15:33:02 -0400

    any comment

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/src/main/java/org/apache/hadoop/mapred/YARNRunner.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/src/main/java/org/apache/hadoop/mapred/YARNRunner.java
@@ -301,7 +301,7 @@ public class YARNRunner implements ClientProtocol {
 	             		   
 	             	   }
 	                }
-	                if(lineReceived!=null && !lineReceived.isEmpty())
+	                //if(lineReceived!=null && !lineReceived.isEmpty())
 	                {
 			                if (lineReceived.startsWith("ww"))//TODO NEED TO HAVE A BETTER WAY TO CLOSE THE THREAD
 								break;
