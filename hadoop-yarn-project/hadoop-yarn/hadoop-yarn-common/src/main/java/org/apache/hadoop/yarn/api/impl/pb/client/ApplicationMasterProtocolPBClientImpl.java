cb7167a6d46f272ca4bde83787da6f4db828d8a2
Bara_Ubuntu64bit <bara@ubuntu.(none)>
2014-11-05 18:00:17 -0500

    any comment

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/api/impl/pb/client/ApplicationMasterProtocolPBClientImpl.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/api/impl/pb/client/ApplicationMasterProtocolPBClientImpl.java
@@ -76,7 +76,7 @@ public class ApplicationMasterProtocolPBClientImpl implements ApplicationMasterP
 	  
 	  for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {System.out.println("ste = "+ste);}
 	  
-	  System.out.println(request.taskAttemptID_IN_AllocateRequest_abstract + request.getTaskAttemptID_IN_AllocateRequest_abstract()
+	  System.out.println("----------"+request.taskAttemptID_IN_AllocateRequest_abstract + request.getTaskAttemptID_IN_AllocateRequest_abstract()
 			  +request.getTaskAttemptID_IN_AllocateRequest_abstract222());
 	  
     AllocateRequestProto requestProto =
