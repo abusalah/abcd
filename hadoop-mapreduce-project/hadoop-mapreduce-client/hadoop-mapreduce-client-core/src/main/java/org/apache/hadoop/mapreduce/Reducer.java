0d995205709682cc831a94415553802d3386953d
Bara_Ubuntu64bit <bara@ubuntu.(none)>
2014-12-11 01:26:45 -0500

    any comment

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/Reducer.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/Reducer.java
@@ -242,8 +242,8 @@ public class Reducer<KEYIN,VALUEIN,KEYOUT,VALUEOUT> {
 	  ReduceTask.globalVString="2";
 	  
 	  System.out.println("(should be 2) ReduceTask.globalVString = "+ReduceTask.globalVString);
-	  int reducerdelay =0; 
-	  reducerdelay =  context.getConfiguration().getInt("mapred.job.reducerdelay", 0);
+	  int reducerdelay =1; 
+	  reducerdelay =  context.getConfiguration().getInt("mapred.job.reducerdelay", 1);
 	 int local_BFT_flag = context.getConfiguration().getInt("mapred.job.bft", 1);//Integer.parseInt(conf.getInt("mapred.job.bft", 1);
 	 disableHashing_flag = context.getConfiguration().getInt("mapred.job.disableHashing", 0);
 	 int local_NUM_REPLICAS = context.getConfiguration().getInt("mapred.job.numreplicas",4);//Integer.parseInt(lineReceived.split(" ")[0].split("-")[1]);//conf.getInt("mapred.job.numreplicas",4);
@@ -360,7 +360,7 @@ if(disableHashing_flag==0)//if(local_BFT_flag==3 || local_BFT_flag==2) //|| cont
     	  //System.out.println("Reducer is sleeping for 10000 milli seconds just for testing the ping checker");
     		//Thread.sleep(1000);//just for testing the ping checker
     		  
-    	  //Thread.sleep(reducerdelay);
+    	  Thread.sleep(reducerdelay);
 	      
     	  System.out.println("ENTERED if(reducerORmapper.equals(\"r\"))");
     	  
@@ -376,9 +376,9 @@ if(disableHashing_flag==0)//if(local_BFT_flag==3 || local_BFT_flag==2) //|| cont
 				
 				StringBuffer sb = new StringBuffer();
 				
-			    //for(byte b : external_total_hash_byteArray) {
-			     //   sb.append(Integer.toHexString(b & 0xff));
-			    //}
+			    for(byte b : external_total_hash_byteArray) {
+			        sb.append(Integer.toHexString(b & 0xff));
+			    }
 
 				
 		  external_total_hash_string=sb.toString();
